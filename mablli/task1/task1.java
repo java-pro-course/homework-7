d830aa434a286ec56e843a62a165511a216cd9d6
Лев Шинкоренко <Lev Shinkorenko>
2022-10-15 10:58:32 +0500

    Add task1.java

--- mablli/task1/task1.java
+++ mablli/task1/task1.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Human firstHuman = new Human();
-        firstHuman.getInfo();
+        Human human1 = new Human();
+        human1.getInfo();
     }
+
  public static Scanner in = new Scanner(System.in);
+}
 
-    static class Human {
-        String gender = "male";
-        String name = "John";
-        int height = 178;
-        String eyesColor = "blue";
-        String hairColor = "dark";
+class Human {
+    String gender = "male";
+    String name = "John";
+    int height = 178;
+    String eyesColor = "blue";
+    String hairColor = "dark";
 
-        public void getInfo() {
-            System.out.println(gender + "; " + name + "; " + height + "; " + eyesColor + "; " + hairColor + ".");
-        }
+    public void getInfo() {
+        System.out.println(gender + "; " + name + "; " + height + "; " + eyesColor + "; " + hairColor + ".");
     }
 }
+
